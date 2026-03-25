de5dd66e663171d8e7c76639b607340612630ec0
Saadbinzahid1 <sbinzahid2@gmail.com>
2026-03-25 22:08:12 +0500

    Added the Finish Screen functionality

---
+++ src/FinishScreen.js
@@ -0,0 +1,19 @@
+export default function FinishScreen({ points, maxPossiblePoints, highscore }) {
+  const percentage = (points / maxPossiblePoints) * 100;
+
+  let emoji;
+  if (percentage === 100) emoji = "🥇";
+  else if (percentage >= 80 && percentage < 100) emoji = "🎉";
+  else if (percentage >= 50 && percentage < 80) emoji = "🌝";
+  else if (percentage >= 0 && percentage < 50) emoji = "🤨";
+  else emoji = "🤦‍♂️";
+  return (
+    <>
+      <p className="result">
+        <span>{emoji}</span> You scored <strong>{points}</strong> out of{" "}
+        {maxPossiblePoints} ({Math.ceil(percentage)}%)
+      </p>
+      <p className="highscore">(Highscore: {highscore} points)</p>
+    </>
+  );
+}
